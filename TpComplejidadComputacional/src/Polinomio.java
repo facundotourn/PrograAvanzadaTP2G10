d5a7e7a5b5754ea81e13cf985ccc113a3804f202
Facundo Tourn <facundotourn@outlook.com>
2019-09-13 19:15:25 -0300

    Implementado potenciaRecursivaPar

--- TpComplejidadComputacional/src/Polinomio.java
+++ TpComplejidadComputacional/src/Polinomio.java
@@ -5,11 +5,12 @@ public class Polinomio {
 
 //La posicion 0 del arreglo de coeficientes contiene el coeficiente de grado n y la posicion n contiene al termino independiente. 
 	public static void main(String[] args) {
-		double[] arrP = { 5, -4, 1 };
+		double[] arrP = { 8, 45, 6, 6, 2 };
 		Polinomio p = new Polinomio(arrP);
 		System.out.println(p.evaluarMSucesivas(7));
 		System.out.println(p.evaluarPow(7));
 		System.out.println(p.evaluarRecursiva(7));
+		System.out.println(p.evaluarRecursivaPar(7));
 	}
 
 	public Polinomio(double[] coeficientes) {
@@ -18,11 +19,10 @@ public class Polinomio {
 	}
 
 	double evaluarMSucesivas(double x) {
-		int i, j;
-		double res = 0, aux;
-		for (i = 0; i <= this.grado; i++) {
-			aux = 1;
-			for (j = 0; j <= this.grado - i - 1; j++) {
+		double res = 0;
+		for (int i = 0; i <= this.grado; i++) {
+			double aux = 1;
+			for (int j = 0; j <= this.grado - i - 1; j++) {
 				aux *= x;
 			}
 
@@ -33,21 +33,33 @@ public class Polinomio {
 
 	double evaluarRecursiva(double x) {
 		double res = 0;
-		for (int i = 0; i <= this.grado; i++) {
-			res += coeficientes[i] * recursiva(x, grado - i);
-		}
+		for (int i = 0; i <= this.grado; i++)
+			res += coeficientes[i] * potenciaRecursiva(x, this.grado - i);
+			
 		return res;
 	}
 
-	private double recursiva(double x, int i) {
-		if (i == 1) {
-			return x;
-		} else if (i == 0) {
-			return 1;
-		} else {
-
-			return x * recursiva(x, i - 1);
-		}
+	private double potenciaRecursiva(double x, int exp) {
+		if (exp == 0) return 1;
+		
+		return x * potenciaRecursiva(x, exp - 1);
+	}
+	
+	double evaluarRecursivaPar(double x) {
+		double res = 0;
+		for (int i = 0; i <= this.grado; i++)
+			res += coeficientes[i] * potenciaRecursivaPar(x, this.grado - i);
+		
+		return res;
+	}
+	
+	private double potenciaRecursivaPar(double x, int exp) {
+		if (exp == 0) return 1;
+		
+		if (exp % 2 == 0) 
+			return potenciaRecursivaPar(x * x, exp / 2);
+		else 
+			return x * potenciaRecursivaPar(x, exp - 1);
 	}
 
 	/*
