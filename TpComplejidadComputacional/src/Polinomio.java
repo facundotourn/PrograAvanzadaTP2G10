ef06401a2237933df5f184d6bcfca07963a8a4f9
jkrasuk <jkrasuk@gylgroup.com>
2019-09-14 09:36:31 -0300

    Fix Merge y evaluarProgDinamica
    Tambien PDF consigna

--- TpComplejidadComputacional/src/Polinomio.java
+++ TpComplejidadComputacional/src/Polinomio.java
@@ -10,11 +10,9 @@ public class Polinomio {
 		System.out.println(p.evaluarMSucesivas(7));
 		System.out.println(p.evaluarPow(7));
 		System.out.println(p.evaluarRecursiva(7));
-<<<<<<< HEAD
 		System.out.println(p.evaluarHorner(7));
-=======
 		System.out.println(p.evaluarRecursivaPar(7));
->>>>>>> d5a7e7a5b5754ea81e13cf985ccc113a3804f202
+		p.evaluarProgDinamica(7);
 	}
 
 	public Polinomio(double[] coeficientes) {
@@ -39,39 +37,54 @@ public class Polinomio {
 		double res = 0;
 		for (int i = 0; i <= this.grado; i++)
 			res += coeficientes[i] * potenciaRecursiva(x, this.grado - i);
-			
+
 		return res;
 	}
 
 	private double potenciaRecursiva(double x, int exp) {
-		if (exp == 0) return 1;
-		
+		if (exp == 0)
+			return 1;
+
 		return x * potenciaRecursiva(x, exp - 1);
 	}
-	
+
 	double evaluarRecursivaPar(double x) {
 		double res = 0;
 		for (int i = 0; i <= this.grado; i++)
 			res += coeficientes[i] * potenciaRecursivaPar(x, this.grado - i);
-		
+
 		return res;
 	}
-	
+
 	private double potenciaRecursivaPar(double x, int exp) {
-		if (exp == 0) return 1;
-		
-		if (exp % 2 == 0) 
+		if (exp == 0)
+			return 1;
+
+		if (exp % 2 == 0)
 			return potenciaRecursivaPar(x * x, exp / 2);
-		else 
+		else
 			return x * potenciaRecursivaPar(x, exp - 1);
 	}
 
+	double evaluarProgDinamica(double x) {
+		double res = 0;
+		double[] arr = new double[this.coeficientes.length];
+
+		// Inicializo el array en 1, para que pueda hacer el producto
+		arr[0] = 1;
+		for (int i = 1; i <= this.grado; i++) {
+			arr[i] = x * arr[i - 1];
+		}
+
+		// Ahora ya tengo las potencias, paso a calcular mi resultado
+		for (int i = 0; i <= this.grado; i++) {
+			res += arr[i] * coeficientes[this.grado - i];
+		}
+		
+		return res;
+	}
+
 	/*
-	 * 
-	 * double evaluarRecursivaPar(double x) { }
-	 * 
-	 * double evaluarProgDinamica(double x) { }
-	 * 
 	 * double evaluarMejorada(double x) { }
 	 */
 	double evaluarPow(double x) {
